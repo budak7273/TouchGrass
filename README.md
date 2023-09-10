923e932f5b60ae36a5179efc51b3b1ef1b6a55e8
Rob B <computerguy440+gh@gmail.com>
2023-09-10 18:30:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Touch Grass Lib
+
+This will Eventually™ offer an optimized cpp call with nice caching things for detecting skylight access, what the player is standing on, etc. but for now it is a mediocrely-optimized blueprint function lib.
---
+++ Source/TouchGrass/Public/TouchGrassLib.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "TouchGrassLib.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOUCHGRASS_API UTouchGrassLib : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
